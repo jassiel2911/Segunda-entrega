08bbe6abae85a47faabbe37b2bbdbcc11466db3c
Brian <42351210+jassiel2911@users.noreply.github.com>
2019-04-19 16:52:49 -0500

    Create Proyecto2.c

---
+++ Proyecto2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "segundasummat.h"
+
+int main (void){
+	int mat1[3][3]={}, mat2[3][3]={};
+	int filas, columnas;
+	CapturarM();
+	void sumar(int a[30][30],int b[30][30],int filas, int columnas);
+	Resultado();	
+	return 0;
+}
